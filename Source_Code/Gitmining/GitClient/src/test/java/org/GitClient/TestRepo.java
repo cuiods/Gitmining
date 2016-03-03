c826d94d70df028897d17ea27ba70a40fc794a90
dxy14 <dxy14@software.nju.edu.cn>
2016-03-03 21:16:12 +0800

    Development: code testing case about user

---
+++ Source_Code/Gitmining/GitClient/src/test/java/org/GitClient/TestRepo.java
@@ -0,0 +1,15 @@
+package org.GitClient;
+
+import junit.framework.TestCase;
+
+public class TestRepo extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
