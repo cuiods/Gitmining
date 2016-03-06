5cfae57381cece93bfbdecf09c9df95cd48ef77b
yyy14 <yyy14@software.nju.edu.cn>
2016-03-06 17:56:42 +0800

    Development: update the UI part'

---
+++ Source_Code/Gitmining/GitClient/src/main/java/edu/nju/git/ui/listener/LabelRepoListener.java
@@ -0,0 +1,48 @@
+package edu.nju.git.ui.listener;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import edu.nju.git.ui.components.table.MyTableLabel;
+import edu.nju.git.ui.controller.UIController;
+/*
+ * listener observed by label in the reposTable or userTable
+ * @auther yyy
+ * @date 2016-03-06 17:55
+ */
+public class LabelRepoListener implements MouseListener{
+	
+	public LabelRepoListener(MyTableLabel label,UIController controller){
+		
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
