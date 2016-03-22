836e8cd43b6ffbdafe9aa3cac8d00e9207cf0d8c
chao14 <chao14@software.nju.edu.cn>
2016-03-22 20:40:21 +0800

    Fix bug: fix ui exception

--- Source_Code/Gitmining/GitClient/src/main/java/edu/nju/git/ui/control/function/RepoListFunction.java
+++ Source_Code/Gitmining/GitClient/src/main/java/edu/nju/git/ui/control/function/RepoListFunction.java
@@ -3,15 +3,32 @@ package edu.nju.git.ui.control.function;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import edu.nju.git.ui.control.GitPanel;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.layout.VBox;
 
-public class RepoListFunction implements Initializable{
+public class RepoListFunction extends GitPanel{
 
 	@FXML private VBox listBox;
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+	@Override
+	public void initPanel(Object[] bundle) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void setChildren(Parent region) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void removeChild() {
+		// TODO Auto-generated method stub
 		
 	}
 
