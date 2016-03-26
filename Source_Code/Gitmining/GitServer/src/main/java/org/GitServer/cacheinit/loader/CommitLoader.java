c38d00878324a22353e4411cbc3025998c412371
dxy14 <dxy14@software.nju.edu.cn>
2016-03-26 14:59:20 +0700

    Development: download commits.

--- Source_Code/Gitmining/GitServer/src/main/java/org/GitServer/cacheinit/loader/CommitLoader.java
+++ Source_Code/Gitmining/GitServer/src/main/java/org/GitServer/cacheinit/loader/CommitLoader.java
@@ -1,5 +1,6 @@
 package org.GitServer.cacheinit.loader;
 
+import org.GitServer.cacheinit.loader.api.APIconfig;
 import org.GitServer.cacheinit.loader.service.AbstractPullIssueCommitLoader;
 
 public class CommitLoader extends AbstractPullIssueCommitLoader{
@@ -9,7 +10,7 @@ public class CommitLoader extends AbstractPullIssueCommitLoader{
 
 	@Override
 	protected String getURL(int page) {
-		//return "https://api.github.com/repos/"+fullname+"/commits"+APIconfig.getClientidandsecret()+"&page="+page;
-		return "http://gitmining.net/api/repository/"+fullname+"/commits?page="+page;
+		return "https://api.github.com/repos/"+fullname+"/commits"+APIconfig.getClientidandsecret()+"&page="+page;
+		//return "http://gitmining.net/api/repository/"+fullname+"/commits?page="+page;
 	}
 }
