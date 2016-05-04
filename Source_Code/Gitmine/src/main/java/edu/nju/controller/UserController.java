9bc1d05cef06ca88c24fadde00ad55e9eb149493
chao14 <chao14@software.nju.edu.cn>
2016-05-04 17:57:09 +0800

    Development: file structure

---
+++ Source_Code/Gitmine/src/main/java/edu/nju/controller/UserController.java
@@ -0,0 +1,7 @@
+package edu.nju.controller;
+
+/**
+ * Created by cuihao on 2016/5/4.
+ */
+public class UserController {
+}
---
+++ Source_Code/Gitmine/src/main/java/edu/nju/dao/RepoService.java
@@ -0,0 +1,7 @@
+package edu.nju.dao;
+
+/**
+ * Created by cuihao on 2016/5/4.
+ */
+public class RepoService {
+}
