08909c3f723893a240ef100f9ca2cfafc52ba626
dbc14 <benchaodong@gmail.com>
2016-05-11 20:22:54 +0800

    Development:recommend frame

--- Source_Code/Gitmine/src/main/java/edu/nju/controller/LoginController.java
+++ Source_Code/Gitmine/src/main/java/edu/nju/controller/LoginController.java
@@ -5,13 +5,15 @@ import edu.nju.model.WebUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by cuihao on 2016/5/4.
@@ -36,49 +38,73 @@ public class LoginController {
     @RequestMapping(value = "/register", method = RequestMethod.GET)
     public String register(Model model){
         model.addAttribute("webUser", new WebUser());
+        model.addAttribute("message", "");
         return "register";
     }
 
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String addNewUser(@Valid WebUser webUser, BindingResult bindingResult){
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String addNewUser(@Valid WebUser webUser, BindingResult bindingResult, Model model){
         if (bindingResult.hasErrors()){
             return "register";
         }
         boolean registerResult = loginImpl.register(webUser.getUserName(), webUser.getPassword(), webUser.getEmail());
         if (registerResult){
-            return "registersuccess";
+            //todo add a new entry to hobby database
+
+            return "registerSuccess";
         }
         else {
-            return "registerfail";
+            model.addAttribute("message", "the username has exist");
+            return "register";
         }
     }
 
 
     @RequestMapping(method = RequestMethod.GET)
     public String login(Model model){
-        //todo protext the password from leaking? security?
-        model.addAttribute("webUser", new WebUser());
+        model.addAttribute("message", "");
         return "login";
     }
 
     /**
      * if login success, save current user information to session and redirect to the recommend page;<br>
      * else back to login page
-     * @param webUser
+     * @param username
+     * @param password
      * @param model
+     * @param session
      * @return
      */
-    @RequestMapping(value = "/verify", method = RequestMethod.POST)
-    public String login(@RequestParam(value = "webUser") WebUser webUser, Model model){
-        boolean loginResult = loginImpl.login(webUser.getUserName(), webUser.getPassword());
+    @RequestMapping(method = RequestMethod.POST)
+    public String login(@RequestParam String username, @RequestParam String password,
+                        Model model, HttpSession session){
+        if (!loginImpl.existName(username)){
+            model.addAttribute("message", "the user do not exist");
+        }
+        boolean loginResult = loginImpl.login(username, password);
         if (loginResult){
-            //todo add user information to session scope for aop to use
-
+            //add user information to session scope for aop to use
+            session.setAttribute("username", username);
             return "redirect:/repo/home";   //default to repo page
         }
         else {
+            model.addAttribute("message", "the password is wrong");
             return "login";
         }
+    }
+
+    @RequestMapping(value = "/interest", method = RequestMethod.GET)
+    public String selectHobby(Model model){
+        List<String> list = new ArrayList<String>();
+        model.addAttribute("list", list);
+        return "selectHobby";
+    }
+
+    @RequestMapping(value = "/interest", method = RequestMethod.POST)
+    public String submitHobby(@RequestParam List list, Model model){
+        //todo set hobby to database
+
+        return "redirect:/repo/home";
     }
 
 }
--- Source_Code/Gitmine/src/main/webapp/WEB-INF/jsp/selectHobby.jsp
+++ Source_Code/Gitmine/src/main/webapp/WEB-INF/jsp/selectHobby.jsp
@@ -2,15 +2,15 @@
   Created by IntelliJ IDEA.
   User: Harry
   Date: 2016/5/11
-  Time: 11:28
+  Time: 11:25
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Register Fail</title>
+    <title>select interest</title>
 </head>
 <body>
-<h1>sorry, the username has existed!</h1>
+
 </body>
 </html>
