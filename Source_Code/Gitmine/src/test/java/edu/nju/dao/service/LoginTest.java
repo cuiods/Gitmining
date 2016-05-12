b099652a0f761e9b62607231a5e49dadad0f733f
chao14 <chao14@software.nju.edu.cn>
2016-05-12 15:30:10 +0800

    Development: junit test

--- Source_Code/Gitmine/src/test/java/edu/nju/dao/service/LoginTest.java
+++ Source_Code/Gitmine/src/test/java/edu/nju/dao/service/LoginTest.java
@@ -1,7 +1,27 @@
 package edu.nju.dao.service;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
 /**
  * Created by cuihao on 2016/5/4.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath*:/META-INF/applicationContext.xml")
+@Transactional
 public class LoginTest {
-}
+    @Resource
+    RegisterDaoService registerDaoService;
+    @Test
+    public void testExistNameTrue() {
+        boolean test = registerDaoService.existName("cuiods");
+        Assert.assertEquals(true,test);
+        boolean test2 = registerDaoService.existName("233");
+        Assert.assertEquals(false,test2);
+    }}
