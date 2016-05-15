5891dd197fabe9a95b014fdbc727bbfd95703758
dxy14 <dxy14@software.nju.edu.cn>
2016-05-16 00:22:18 +0800

    Development: repo-list js  .

--- Source_Code/Gitmine/src/main/webapp/js/repo-list.js
+++ Source_Code/Gitmine/src/main/webapp/js/repo-list.js
@@ -1,19 +1,51 @@
 /**
  * Created by darxan on 2016/5/15.
  */
-$(document).ready(
-    function () {
-       var gridsFather =  $("#news-grids");
+
+var Repo = {
+
+    initListeners: function () {
+
+    },
+    generateGrids: function () {
+        var gridsFather =  $("#news-grids");
         var grids = gridsFather.find('.news-grid');
-       var lastGrid = grids.eq(0);
+        var lastGrid = grids.eq(0);
         console.log(lastGrid);
         var count = grids.length;
         console.log(count);
-
+        var clear = $("<div class=\"clearfix\"> </div>" );
         for (var i=count; i<16; i++){
             var tempGrid = lastGrid.clone(true);
             gridsFather.append(tempGrid);
+            if(i%4==3){
+                gridsFather.append(clear.clone());
+            }
         }
+    },
+
+
+    updateData: function (object) {
 
     }
+};
+
+$(document).ready(
+    function () {
+        Repo.generateGrids();
+        Repo.initListeners();
+        Repo.updateData(1);
+    }
 );
+
+
+function request( isPost,url,data, callback) {
+    var json;
+    if(isPost){
+        json =  $.post(url,data,callback);
+    }else{
+        url += '?'+$.param(data,true);
+        json =  $.get(url,callback);
+    };
+    return $.parseJSON(json);
+}
