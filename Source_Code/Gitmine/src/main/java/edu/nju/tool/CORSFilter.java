e331373de5fca9b67371ad00e504994df729643a
dxy14 <dxy14@software.nju.edu.cn>
2016-05-18 08:59:50 +0800

    Development:html:radio  .

---
+++ Source_Code/Gitmine/src/main/java/edu/nju/tool/CORSFilter.java
@@ -0,0 +1,13 @@
+package edu.nju.tool;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by darxan on 2016/5/18.
+ */
+@Component
+public class CORSFilter
+//        implements ComponentScan.Filter
+{
+
+}
--- Source_Code/Gitmine/src/main/webapp/js/repo-list.js
+++ Source_Code/Gitmine/src/main/webapp/js/repo-list.js
@@ -1,23 +1,22 @@
 /**
  * Created by darxan on 2016/5/15.
  */
-var location_port = '127.0.0.1:8080/';
+var location_port = 'http://localhost:8080';
 var Repo = {
 
 
     // gridsFatherNode: $("#news-grids"),
     // gridNode: this.gridsFatherNode.find(".news-grid").eq(0),
-
     initListeners: function () {
-
-
-
     },
-    generateGrids: function () {
+
+    init: function () {
         this.gridsFather =  $("#news-grids");
         this.lastGrid = this.gridsFather.children(".news-grid");
         this.clear = $("<div class=\"clearfix\"> </div>" );
         this.gridsFather.empty();
+        this.url = location_port+"/search";
+
         for (var i=0; i<16; i++){
             var tempGrid = this.lastGrid.clone(true);
             this.gridsFather.append(tempGrid);
@@ -30,34 +29,52 @@ var Repo = {
 
     updateData: function (object) {
 
+
+        // var object = $.parseJSON(data);
+
         this.gridsFather.empty();
         $.each(object, function (i, n)
         {
+            console.log(n);
             var element = this.lastGrid.clone();
+            this.gridsFather.append(element);
+            if(i%4==3){
+                this.gridsFather.append(this.clear.clone());
+            }
 
         });
+    },
+
+    updateList: function (pageNum) {
+        var url = location_port+'/repo/list'+"?pageNum="+pageNum;
+        var oAjax = null;
+        if(window.XMLHttpRequest){
+            oAjax = new XMLHttpRequest();
+        }else{
+            oAjax = new ActiveXObject('Microsoft.XMLHTTP');
+        }
+        oAjax.open('GET', url, true);
+        oAjax.send();
+
+        oAjax.onreadystatechange=function(){
+            if(oAjax.readyState==4){
+                if(oAjax.status==200){
+                    console.log(oAjax.responseText);
+                }else{
+                    console.log('filed');
+                }
+            }
+        };
     }
 };
 
 $(document).ready(
     function () {
-        Repo.generateGrids();
+        Repo.init();
         Repo.initListeners();
-        Repo.updateData(request(1,1,1,1));
+        Repo.updateList(1);
+        
+
     }
 );
 
-
-function request( isPost,url,data, callback) {
-    var json;
-    if(isPost){
-        json =  $.post(url,data,callback);
-    }else{
-        url += location_port+'?'+$.param(data,true);
-        json =  $.get(url,callback);
-    };
-
-    var object = $.parseJSON(json);
-    console.log(object);
-    return object;
-}
