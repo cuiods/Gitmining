3fe0af6cb99ca446454ae6ccf169fa0d871ce837
yyy14 <yyy14@software.nju.edu.cn>
2016-05-18 21:09:41 +0800

    Development:update the user part

--- Source_Code/Gitmine/src/main/webapp/js/userDetail.js
+++ Source_Code/Gitmine/src/main/webapp/js/userDetail.js
@@ -6,7 +6,31 @@ $(document).ready(function() {
     var userName=document.getElementById("userName");
     userName.innerHTML=getQueryString('userName');
     $().UItoTop({ easingType: 'easeOutQuart' });
+
+    $.ajax({
+        type:'POST',
+        url:'/user'+userName.innerHTML,
+        success:function(data){
+            document.getElementById("userDetail_repos").innerHTML=data.publicRepo;
+            document.getElementById("userDetail_following").innerHTML=data.following;
+            document.getElementById("userDetail_followed").innerHTML=data.follower;
+            document.getElementById("userDetail_gists").innerHTML=data.publicGist;
+            document.getElementById("blog").innerHTML=data.blog;
+            document.getElementById("email").innerHTML=data.email;
+            document.getElementById("company").innerHTML=data.company;
+            document.getElementById("location").innerHTML=data.location;
+
+        },
+        error:function(){
+            alert("controller return result unknown error!");
+        }
+    })
+
+
+
 });
+
+
 
 //to get the parameter transfered by url
 function getQueryString(name) {
