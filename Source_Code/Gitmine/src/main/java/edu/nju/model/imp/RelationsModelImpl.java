944e011daea039b5253f1bdb723ea8b698d6ce3f
dxy14 <dxy14@software.nju.edu.cn>
2016-06-02 20:23:58 +0800

    Development: relation graph.

--- Source_Code/Gitmine/src/main/java/edu/nju/model/imp/RelationsModelImpl.java
+++ Source_Code/Gitmine/src/main/java/edu/nju/model/imp/RelationsModelImpl.java
@@ -6,6 +6,7 @@ import edu.nju.dao.service.SecRepoDaoService;
 import edu.nju.dao.service.SecUserDaoService;
 import edu.nju.dao.service.UserDaoService;
 import edu.nju.entity.TblRepo;
+import edu.nju.entity.TblUser;
 import edu.nju.model.pojo.RelationVO;
 import edu.nju.model.service.RelationsModelService;
 import edu.nju.model.service.RepoModelService;
@@ -14,7 +15,9 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by darxan on 2016/6/1.
@@ -31,20 +34,60 @@ public class RelationsModelImpl implements RelationsModelService{
     @Resource
     private SecRepoDaoService secRepoDaoService;
 
-    public List<RelationVO> getRelationsByUser(String username){
-        //get user own repos
-        //ger user contribute repos
-        List<RelationVO> result = new ArrayList<>();
-        List<TblRepo> tblRepos = userDaoService.getUserOwnRepos(username, SortType.Repo_Star,0,100);
-        tblRepos.forEach(tblRepo -> {
-            result.add(new RelationVO(username,RelationVO.REPO,tblRepo.getName(),null));
+    private List<RelationVO> result;
+    private Map<String,RelationVO> userRelations;
+    private Map<String,RelationVO> repoRelations;
+
+    public List<RelationVO> getRelationsByUser(final String username){
+
+        result = new ArrayList<>();
+        userRelations = new HashMap<>();
+        repoRelations = new HashMap<>();
+
+        List<String> userDepend = new ArrayList<>();
+        RelationVO userRelation = new RelationVO(username,RelationVO.USER,username,userDepend);
+        result.add(userRelation);
+        userRelations.put(username,userRelation);
+
+        //user owner repositories
+        List<TblRepo> userOwnRepos = userDaoService.getUserOwnRepos(username, SortType.Repo_Star,0,100);
+        if(userOwnRepos.size()==0){
+            throw  new NullPointerException();
+        }
+        userOwnRepos.forEach(tblRepo -> {
+            List<String> depend = new ArrayList<>(1);
+            final String fullname = tblRepo.getOwnerName()+"/"+tblRepo.getName();
+            depend.add(username);
+            RelationVO repoRelation = new RelationVO(username,RelationVO.REPO,fullname,depend);
+             if(!repoRelations.containsKey(fullname)){
+                 result.add(repoRelation);
+                 repoRelations.put(fullname,repoRelation);
+             }
+            List<String> repoContributors =
+                    repoDaoService.getRepoContributor(tblRepo.getOwnerName(),tblRepo.getName());
+            repoContributors.forEach(contributor->{
+                if(!userRelations.containsKey(contributor)){
+                    List<String> contributorDepend = new ArrayList<>(1);
+                    contributorDepend.add(fullname);
+                    RelationVO userRelationVO = new RelationVO(contributor,RelationVO.USER,contributor,contributorDepend);
+                    result.add(userRelationVO);
+                    userRelations.put(contributor,userRelationVO);
+                }
+
+            });
         });
 
         //
         return result;
     }
 
-    public List<RelationVO> getRelationsByRepo(String ownername, String reponame){
+
+
+
+
+
+
+    public List<RelationVO> getRelationsByRepo(final String ownername,final String reponame){
         return null;
     }
 }
