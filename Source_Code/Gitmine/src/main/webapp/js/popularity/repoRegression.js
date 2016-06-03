b38a4d0b728447de0a8b4aa9198f269263bc3178
chao14 <chao14@software.nju.edu.cn>
2016-06-03 17:00:37 +0800

    Development: popularity ui structure

---
+++ Source_Code/Gitmine/src/main/webapp/js/popularity/repoRegression.js
@@ -0,0 +1,116 @@
+/**
+ * Created by cuihao on 2016/6/3.
+ */
+$(document).ready(function () {
+    var popu_star = echarts.init(document.getElementById('gra-popularity-star'));
+    popu_star.showLoading();
+    $.ajax({
+        type:"GET",
+        url:"/popularity/star",
+        success:function (stardata) {
+            var dataAll = [
+                stardata.fork,
+                stardata.watcher
+            ];
+            var statdata = stardata.stat;
+
+            var markLineOpt = {
+                animation: false,
+                label: {
+                    normal: {
+                        formatter: statdata[0],
+                        textStyle: {
+                            align: 'right'
+                        }
+                    }
+                },
+                lineStyle: {
+                    normal: {
+                        type: 'solid'
+                    }
+                },
+                tooltip: {
+                    formatter: statdata[0]
+                },
+                data: [[{
+                    coord: [0, statdata[1]],
+                    symbol: 'none'
+                }, {
+                    coord: [1000, statdata[2]],
+                    symbol: 'none'
+                }]]
+            };
+
+            var markLineOpt2 = {
+                animation: false,
+                label: {
+                    normal: {
+                        formatter: statdata[3],
+                        textStyle: {
+                            align: 'right'
+                        }
+                    }
+                },
+                lineStyle: {
+                    normal: {
+                        type: 'solid'
+                    }
+                },
+                tooltip: {
+                    formatter: statdata[3]
+                },
+                data: [[{
+                    coord: [0, statdata[4]],
+                    symbol: 'none'
+                }, {
+                    coord: [1000, statdata[5]],
+                    symbol: 'none'
+                }]]
+            };
+
+            option = {
+                title: {
+                    text: 'Star with fork and watcher number',
+                    x: 'center',
+                    y: 0
+                },
+                grid: [
+                    {x: '7%', y: '7%', width: '38%', height: '70%'},
+                    {x2: '7%', y: '7%', width: '38%', height: '70%'}
+                ],
+                tooltip: {
+                    formatter: 'Group {a}: ({c})'
+                },
+                xAxis: [
+                    {gridIndex: 0, min:0, max:1000},
+                    {gridIndex: 1, min:0, max:1000}
+                ],
+                yAxis: [
+                    {gridIndex: 0, min:0, max:200},
+                    {gridIndex: 1, min:0, max:1000}
+                ],
+                series: [
+                    {
+                        name: 'fork',
+                        type: 'scatter',
+                        xAxisIndex: [0],
+                        yAxisIndex: [0],
+                        data: dataAll[0],
+                        markLine: markLineOpt
+                    },
+                    {
+                        name: 'watcher',
+                        type: 'scatter',
+                        xAxisIndex: [1],
+                        yAxisIndex: [1],
+                        data: dataAll[1],
+                        markLine: markLineOpt2
+                    }
+                ]
+            };
+
+            popu_star.hideLoading();
+            popu_star.setOption(option);
+        }
+    });
+})
