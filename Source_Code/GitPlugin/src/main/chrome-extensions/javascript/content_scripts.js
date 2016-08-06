e2fd2e6211777d3f22c099d32703ffdc508f3a49
dbc14 <benchaodong@gmail.com>
2016-08-06 13:50:25 +0800

    popup window 位置不太对

--- Source_Code/GitPlugin/src/main/chrome-extensions/javascript/content_scripts.js
+++ Source_Code/GitPlugin/src/main/chrome-extensions/javascript/content_scripts.js
@@ -13,9 +13,39 @@ function addPopupToPage() {
         var watch = $(".repohead-details-container ul.pagehead-actions li:nth-child(1)");
         var star = $(".repohead-details-container ul.pagehead-actions li:nth-child(2)");
         var fork = $(".repohead-details-container ul.pagehead-actions li:nth-child(3)");
-        $(watch).append("<div class='popup' id='watch_tip'> <div>Hello guys !</div> <svg height='50' width='100'> <path d='M0 50 L40 0 L65 0 Z' /> Sorry, your browser does not support inline SVG. </svg> </div>");
-        $(star).append("<div class='popup' id='star_tip'> <div>Hello guys !</div> <svg height='50' width='100'> <path d='M0 50 L40 0 L65 0 Z' /> Sorry, your browser does not support inline SVG. </svg> </div>");
-        $(fork).append("<div class='popup' id='fork_tip'> <div>Hello guys !</div> <svg height='50' width='100'> <path d='M0 50 L40 0 L65 0 Z' /> Sorry, your browser does not support inline SVG. </svg> </div>");
+        watch.hover(
+            function (e) {
+                $("<div class='popup' id='watch_tip'> <div>Hello guys !</div> <svg height='50' width='100'> <path d='M0 50 L40 0 L65 0 Z' /> Sorry, your browser does not support inline SVG. </svg> </div>")
+                    .css("top", e.pageX + 20)
+                    .css("left", e.pageY + 10)
+                    .appendTo(watch);
+            },
+            function () {
+                $("#watch_tip").remove();
+            }
+        );
+        star.hover(
+            function (e) {
+                $("<div class='popup' id='star_tip'> <div>Hello guys !</div> <svg height='50' width='100'> <path d='M0 50 L40 0 L65 0 Z' /> Sorry, your browser does not support inline SVG. </svg> </div>")
+                    .css("top", e.pageX + 20)
+                    .css("left", e.pageY + 10)
+                    .appendTo(star);
+            },
+            function () {
+                $("#star_tip").remove();
+            }
+        );
+        fork.hover(
+            function (e) {
+                $("<div class='popup' id='fork_tip'> <div>Hello guys !</div> <svg height='50' width='100'> <path d='M0 50 L40 0 L65 0 Z' /> Sorry, your browser does not support inline SVG. </svg> </div>")
+                    .css("top", e.pageX + 20)
+                    .css("left", e.pageY + 10)
+                    .appendTo(fork);
+            },
+            function () {
+                $("#fork_tip").remove();
+            }
+        );
 
     }
 }
