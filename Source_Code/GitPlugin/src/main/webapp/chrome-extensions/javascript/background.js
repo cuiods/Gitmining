253379b94425b01e3bdc316c42aaefda53761ee6
dbc14 <benchaodong@gmail.com>
2016-08-08 20:34:33 +0800

    还有一个小bug需要解决

--- Source_Code/GitPlugin/src/main/webapp/chrome-extensions/javascript/background.js
+++ Source_Code/GitPlugin/src/main/webapp/chrome-extensions/javascript/background.js
@@ -1,3 +1,69 @@
 /**
  * Created by Harry on 2016/8/4.
  */
+
+function getServerIP() {
+    return "http://127.0.0.1";
+}
+
+function compareWatch(watchCount, port) {
+    $.ajax(
+        {
+            type: "GET",
+            url: getServerIP()+"/compare/watch",
+            data: {num: watchCount},
+            success: function (ratio) {
+                port.postMessage({
+                    dataType: "watch",
+                    ratio: ratio
+                });
+            }
+        }
+    );
+}
+
+function compareStar(starCount, port) {
+    $.ajax({
+        type: "GET",
+        url: getServerIP()+"/compare/star",
+        data: {num: starCount},
+        success: function (ratio) {
+            port.postMessage({
+                dataType: "star",
+                ratio: ratio
+            });
+        }
+    });
+}
+
+function compareFork(forkCount, port) {
+    $.ajax({
+        type: "GET",
+        url: getServerIP()+"/compare/watch",
+        data: {num: forkCount},
+        success: function (ratio) {
+            port.postMessage({
+                dataType: "fork",
+                ratio: ratio
+            });
+        }
+    });
+}
+
+//建立一个缓冲区用于存储一些运算得到的ratio，避免重复计算
+//todo
+
+//监听来自content scripts的连接请求
+chrome.runtime.onConnect.addListener(function (port) {
+    console.log("port " + port.name + " connet to background.");
+    console.log("the sender url is: " + port.sender.url);
+    port.onMessage.addListener(function (msg) { //msg is a json
+
+        if (msg.theme == "compare") {
+            compareWatch(msg.watch, port);
+            compareStar(msg.star, port);
+            compareFork(msg.fork, port);
+        }
+
+    });
+});
--- Source_Code/GitPlugin/src/main/webapp/chrome-extensions/javascript/content_scripts.js
+++ Source_Code/GitPlugin/src/main/webapp/chrome-extensions/javascript/content_scripts.js
@@ -1,16 +1,9 @@
 /**
  * Created by Harry on 2016/8/5.
  */
-var watchCompare = -1.0;
-var starCompare = -1.0;
-var forkCompare = -1.0;
-
-function getServerAddr() {
-    return "http://127.0.0.1";
-}
 
 function getLoadSvg() {
-    return '<div class="loading_icon"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve"> <path fill="#000" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z"> <animateTransform attributeType="xml"attributeName="transform"type="rotate"from="0 25 25"to="360 25 25"dur="0.6s"repeatCount="indefinite"/> </path> </svg></div>>';
+    return '<div class="loading_area"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve"> <path fill="#000" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z"> <animateTransform attributeType="xml"attributeName="transform"type="rotate"from="0 25 25"to="360 25 25"dur="0.6s"repeatCount="indefinite"/> </path> </svg></div>>';
 }
 
 function addPopupToPage() {
@@ -26,90 +19,49 @@ function addPopupToPage() {
         var watch = lis[0];
         var star = lis[1];
         var fork = lis[2];
-        $(watch).hover(
-            function (e) {
-                $("<div class='popup' id='watch_tip'> <div>watch count beat</div></div>")
-                    .css("top", e.pageY + 20)
-                    .css("left", e.pageX)
-                    .appendTo("body");
-                $(getLoadSvg()).appendTo($(".popup"));
-                console.log("watch type: "+ watch);
-                var watch_count = watch.getElementsByClassName("social-count")[0].innerHTML;
-                $.ajax(
-                    {
-                        url: getServerAddr()+"/compare/watch",
-                        data: {
-                            num: watch_count
-                        },
-                        type: "GET",
-                        dataType: "json",
-                        success: function(rateData){
-                            $(".loading_icon").innerHTML = rateData;
-                        }
-                    }
-                )
-            },
-            function () {
-                $("#watch_tip").remove();
-            }
-        );
-        $(star).hover(
-            function (e) {
-                $("<div class='popup' id='star_tip'> <div>star count beat</div></div>")
-                    .css("top", e.pageY + 20)
-                    .css("left", e.pageX)
-                    .appendTo("body");
-                $(getLoadSvg()).appendTo($(".popup"));
-                var star_count = star.getElementsByClassName("social-count")[0].innerHTML;
-                $.ajax(
-                    {
-                        url: getServerAddr()+"/compare/watch",
-                        data: {
-                            num: star_count
-                        },
-                        type: "GET",
-                        dataType: "json",
-                        success: function(rateData){
-                            $(".loading_icon").innerHTML = rateData;
-                        }
-                    }
-                )
-            },
-            function () {
-                $("#star_tip").remove();
-            }
-
-        );
-        $(fork).hover(
-            function (e) {
-                $("<div class='popup' id='fork_tip'> <div>fork count beat</div></div>")
-                    .css("top", e.pageY + 20)
-                    .css("left", e.pageX)
-                    .appendTo("body");
-                $(getLoadSvg()).appendTo($(".popup"));
-                var fork_count = fork.getElementsByClassName("social-count")[0].innerHTML;
-                $.ajax(
-                    {
-                        url: getServerAddr()+"/compare/watch/watch",
-                        data: {
-                            num: fork_count
-                        },
-                        type: "GET",
-                        dataType: "json",
-                        success: function(rateData){
-                            $(".loading_icon").innerHTML = rateData;
-                        }
-                    }
-                )
-            },
-            function () {
-                $("#fork_tip").remove();
-            }
-        );
+        $(watch).append("<div class='popup' id='watch_tip'> <div>watch count beat</div>"+getLoadSvg()+"</div>");
+        $(star).append("<div class='popup' id='star_tip'> <div>star count beat</div>"+getLoadSvg()+"</div>");
+        $(fork).append("<div class='popup' id='fork_tip'> <div>fork count beat</div>"+getLoadSvg()+"</div>");
 
     }
 }
 
+
+function loadCompareData() {
+    var ownerName = $("#js-repo-pjax-container > div.pagehead.repohead.instapaper_ignore.readability-menu.experiment-repo-nav > div.container.repohead-details-container > h1 > span.author > a")[0].innerHTML;
+    var repoName = $("#js-repo-pjax-container > div.pagehead.repohead.instapaper_ignore.readability-menu.experiment-repo-nav > div.container.repohead-details-container > h1 > strong > a")[0].innerHTML;
+    var watchCount = $("#js-repo-pjax-container > div.pagehead.repohead.instapaper_ignore.readability-menu.experiment-repo-nav > div.container.repohead-details-container > ul > li:nth-child(1) > form > div.select-menu.js-menu-container.js-select-menu > a.social-count.js-social-count")[0].innerHTML.trim();
+    var starCount = $("#js-repo-pjax-container > div.pagehead.repohead.instapaper_ignore.readability-menu.experiment-repo-nav > div.container.repohead-details-container > ul > li:nth-child(2) > div > form.unstarred > a")[0].innerHTML.trim();
+    var forkCount = $("#js-repo-pjax-container > div.pagehead.repohead.instapaper_ignore.readability-menu.experiment-repo-nav > div.container.repohead-details-container > ul > li:nth-child(3) > a")[0].innerHTML.trim();
+
+    var port = chrome.runtime.connect({name: ownerName+"/"+repoName});
+    port.onMessage.addListener(function (msg) {
+        console.log("back msg: "+msg.ratio);
+        if (msg.dataType == "watch"){
+            $("#watch_tip > .loading_area").text(msg.ratio);
+        }
+        else if (msg.dataType == "star"){
+            $("#star_tip > .loading_area").text(msg.ratio);
+        }
+        else if (msg.dataType == "fork"){
+            $("#fork_tip > .loading_area").text(msg.ratio);
+        }
+    });
+    port.postMessage({
+        theme: "compare",
+        watch: watchCount,
+        star: starCount,
+        fork: forkCount
+    });
+}
+
+function listen() {
+    addPopupToPage();
+    loadCompareData();
+}
+
 $(function () {
     addPopupToPage();
+    loadCompareData();
+
 });
