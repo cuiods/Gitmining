8a1dcf6b2f79d9cf9350078a07bf3682b8032450
yyy14 <yyy14@software.nju.edu.cn>
2016-09-01 21:58:54 +0800

    just//

--- Source_Code/GitPlugin/src/test/edu/nju/dao/impl/InfoDaoImplTest.java
+++ Source_Code/GitPlugin/src/test/edu/nju/dao/impl/InfoDaoImplTest.java
@@ -1,37 +1,37 @@
-package edu.nju.dao.impl;
-
-import edu.nju.dao.InfoDao;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import javax.annotation.Resource;
-
-import static org.junit.Assert.*;
-
-/**
- * Created by cuihao on 2016/8/6.
- */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:/META-INF/applicationContext.xml"})
-public class InfoDaoImplTest {
-
-    @Resource
-    private InfoDao infoDao;
-
-    @Test
-    public void getRepoId() throws Exception {
-    }
-
-    @Test
-    public void getCommentsByName() throws Exception {
-        System.out.println(infoDao.getCommentsByName("benalavi","cookbooks",10,1));
-    }
-
-    @Test
-    public void getNewsByName() throws Exception {
-
-    }
-
-}
+//package edu.nju.dao.impl;
+//
+//import edu.nju.dao.InfoDao;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//import javax.annotation.Resource;
+//
+//import static org.junit.Assert.*;
+//
+///**
+// * Created by cuihao on 2016/8/6.
+// */
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = {"classpath:/META-INF/applicationContext.xml"})
+//public class InfoDaoImplTest {
+//
+//    @Resource
+//    private InfoDao infoDao;
+//
+//    @Test
+//    public void getRepoId() throws Exception {
+//    }
+//
+//    @Test
+//    public void getCommentsByName() throws Exception {
+//        System.out.println(infoDao.getCommentsByName("benalavi","cookbooks",10,1));
+//    }
+//
+//    @Test
+//    public void getNewsByName() throws Exception {
+//
+//    }
+//
+//}
