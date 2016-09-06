a9cac8a4a60ca56b661cd3b5bd3cb1c39a960d86
yyy14 <yyy14@software.nju.edu.cn>
2016-09-06 23:38:29 +0800

    UI:modification

--- Source_Code/GitPlugin/src/main/webapp/chrome-extensions/javascript/content_scripts.js
+++ Source_Code/GitPlugin/src/main/webapp/chrome-extensions/javascript/content_scripts.js
@@ -10,7 +10,7 @@ var star_ratio = -1.0;
 var fork_ratio = -1.0;
 
 function getLoadSvg() {
-    return '<div class="loading_area"><svg class="loader" version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve"> <path fill="coral" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z"> <animateTransform attributeType="xml"attributeName="transform"type="rotate"from="0 25 25"to="360 25 25"dur="0.6s"repeatCount="indefinite"/></path></svg></div>';
+    return '<div class="loading_area"><svg class="loader" version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"width="20px" height="20px" viewBox="0 0 20 20" style="enable-background:new 0 0 20 20;" xml:space="preserve"> <path fill="rgb(53,150,227)" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z"> <animateTransform attributeType="xml"attributeName="transform"type="rotate"from="0 25 25"to="360 25 25"dur="0.6s"repeatCount="indefinite"/></path></svg></div>';
 }
 
 function addPopupToPage() {
