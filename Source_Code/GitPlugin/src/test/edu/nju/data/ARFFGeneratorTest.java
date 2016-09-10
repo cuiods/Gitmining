ac2ac367db8e7b164f39fd8b576e47129b9edd69
cuiods <cuihaoemail@163.com>
2016-09-10 08:02:43 +0800

    data

--- Source_Code/GitPlugin/src/test/edu/nju/data/ARFFGeneratorTest.java
+++ Source_Code/GitPlugin/src/test/edu/nju/data/ARFFGeneratorTest.java
@@ -28,8 +28,8 @@ public class ARFFGeneratorTest {
 
     @Test
     public void generateArffFile() throws Exception {
-        Instances instances = arffGenerator.generatePopularInstanceFromDB(1000,10);
-        arffGenerator.generateArffFile(instances,"repo_test.arff");
+        Instances instances = arffGenerator.genetateActivityInstanceFromDB(0,100000);
+        arffGenerator.generateArffFile(instances,"/wekaFile/repo_activity.arff");
     }
 
     @Test
